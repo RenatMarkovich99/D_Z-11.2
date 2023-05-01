a2e570ae005c4d9990109677e2009e140cbe2f18
Renat <matador.99@mail.ru>
2023-05-01 18:26:58 +0500

    "функция заглавных букв" "Функция заглавные буквы у каждого слова"

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def upper_string(string):
+    """
+    Делаем все буквы заглавными
+    """
     return string.upper()
+
+
+def capitalize_words(string):
+    """Пишем с  заглавной
+       буквы все слова в строке
+       """
+    return string.title()
